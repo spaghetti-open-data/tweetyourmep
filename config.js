d31bd3cf66ea062359fdca70976a8d07c4c3350d
Paolo Mainardi (twinbit) <paolo@twinbit.it>
2012-09-23 21:53:23 +0300

    countries taken from view-source:http://www.epnewshub.eu/#/template=contributors&mp=2-0-0  rows 108

--- config.js
+++ config.js
@@ -29,6 +29,7 @@ var config = {
     mep_twitterUserName: String,
   }, {
     autoIndex : true
-  })
+  }),
+  countries: {"AT":"Austria","BE":"Belgium","BG":"Bulgaria","CY":"Cyprus","CZ":"Czech Republic","DK":"Denmark","EE":"Estonia","FI":"Finland","FR":"France","DE":"Germany","GR":"Greece","HU":"Hungary","IE":"Ireland","IT":"Italy","LV":"Latvia","LT":"Lithuania","LU":"Luxembourg","MT":"Malta","NL":"Netherlands","PL":"Poland","PT":"Portugal","RO":"Romania","SK":"Slovakia","SI":"Slovenia","ES":"Spain","SE":"Sweden","GB":"United Kingdom"},
 }
 module.exports = config;
