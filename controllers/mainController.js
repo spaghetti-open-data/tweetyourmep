3c1a068c12e2a73d0fe40b27ab51a9f253d2a56d
Alfredo <seralf@gmail.com>
2012-09-26 09:02:45 +0200

    aggiunto l'hook alla funzione 'findByCriteria'

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -5,22 +5,49 @@ module.exports = function() {
 
 
   var self = {
-    indexAction : function (req,res) {
+    
+   indexAction : function (req,res) {
+	
+	// paraemtri letti dalla request. Se non sono presenti uso il default!
+	var limit = (req.query.limit) ? req.query.limit : 800;
+	var offset = (req.query.offset) ? req.query.offset : 0;
+	   
       // @todo move this to general options ?
       options = { 
         // @todo hardcoded number just for test, this needs to be refactored to support pagination
-        'limit': 800,
+        'limit': limit,
+	'offset': offset,
         'sort_attrib': 'mep_lastName',
         'sort_type': 'asc'
       };
 
+      
+      
+      
+      
       // @todo handle get parameters
       // @todo just use the name to make a test
+      /* ricerca modificata
       if (req.query.mep_name) {
         name = req.query.mep_name;
         meps = model.findByName(name, function(meps) {
           res.render('index', { config: config, meps: meps, req: req});
         });
+      }
+      */
+      if (req.query.mep_name || req.query.mep_country) {
+	 
+console.log("-------------------------------------------------------")
+console.log(req.query)
+console.log("-------------------------------------------------------")	      
+	      
+        name = req.query.mep_name;
+	country = req.query.mep_country;
+	// TODO: sostituire i parametri con un oggetto options modificato solo sui valori interessati
+        meps = model.findByCriteria(name, country, 800, 0, function(meps) {
+          res.render('index', { config: config, meps: meps, req: req});
+        });
+	
       }
       else {
         // @todo check if we can stream the data after the page render, here we hang the page loading!
