7954c4f37fdd3681acd05169dfe4151a1206b2e2
Paolo Mainardi (twinbit) <paolo@twinbit.it>
2012-10-01 02:24:54 +0300

    lazy loading

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -14,11 +14,12 @@
       });
 
     // lazy load images
-    /*
-    $("img.lazy").lazyload({
-     event: "scrollstop"
+    
+    $("img.lazy").show().lazyload({
+       effect : "fadeIn",
+       threshold : 500
     });
-    */
+  
 
     /*
     var countries = {"AT":"Austria","BE":"Belgium","BG":"Bulgaria","CY":"Cyprus","CZ":"Czech Republic","DK":"Denmark","EE":"Estonia","FI":"Finland","FR":"France","DE":"Germany","GR":"Greece","HU":"Hungary","IE":"Ireland","IT":"Italy","LV":"Latvia","LT":"Lithuania","LU":"Luxembourg","MT":"Malta","NL":"Netherlands","PL":"Poland","PT":"Portugal","RO":"Romania","SK":"Slovakia","SI":"Slovenia","ES":"Spain","SE":"Sweden","GB":"United Kingdom"};
