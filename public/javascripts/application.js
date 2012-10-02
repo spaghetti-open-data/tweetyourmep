6075efdbcbc884ea3dfd64a056a54dd9ad22c7ec
Paolo Mainardi (twinbit) <paolo@twinbit.it>
2012-10-02 23:06:59 +0300

    refactoring italian metatags

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -19,14 +19,13 @@
        threshold : 500
     });  
 
-    /*
-    var countries = {"AT":"Austria","BE":"Belgium","BG":"Bulgaria","CY":"Cyprus","CZ":"Czech Republic","DK":"Denmark","EE":"Estonia","FI":"Finland","FR":"France","DE":"Germany","GR":"Greece","HU":"Hungary","IE":"Ireland","IT":"Italy","LV":"Latvia","LT":"Lithuania","LU":"Luxembourg","MT":"Malta","NL":"Netherlands","PL":"Poland","PT":"Portugal","RO":"Romania","SK":"Slovakia","SI":"Slovenia","ES":"Spain","SE":"Sweden","GB":"United Kingdom"};
-    var options = '';
-    for (var key in countries) {
-      options += '<option value="' + key + '">' + countries[key] + '</option>';
-    }
-    $("select#mep_country").html(options);
-    */
-
+    $("#mep_country").select2({
+      placeholder: "Select a State",
+      allowClear: true
+    });
+    $("#mep_faction").select2({
+      placeholder: "Select a Faction",
+      allowClear: true
+    });
   })
 }(window.jQuery)
